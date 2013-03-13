adac27dd0e802335edf14a09d5a8048714130d7c
Mark <m@rkw.me>
2013-03-13 15:46:40 +0000

    initial commit

---
+++ config/common.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'params' => array(
+		'menu_bar_items' => array(
+			'add_patient' => array(
+				'title' => 'Add Patient',
+				'uri' => 'AddPatient/default/create',
+				'position' => 22,
+			),
+		),
+	)
+);
+		
